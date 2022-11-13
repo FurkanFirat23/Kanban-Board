c04ee977e94e84e71bf394eef131e5c51dafac3b
FURKAN FIRAT <furkan.0096@gmail.com>
2022-11-13 03:37:42 +0300

    firebase deploy

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { serverTimestamp, initializeFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_VERCEL_API_KEY,
-  authDomain: process.env.REACT_APP_VERCEL_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_VERCEL_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_VERCEL_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_VERCEL_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_VERCEL_APP_ID,
-  measurementId: process.env.REACT_APP_VERCEL_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_VERCEL_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
